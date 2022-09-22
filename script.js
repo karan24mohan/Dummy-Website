ec2c08232d83393741c87e4e73105d4cae79048b
Karan <karanmohan2410@gmail.com>
2022-09-22 17:16:10 +0530

    Worked futher with the development

--- script.js
+++ script.js
@@ -4,3 +4,56 @@ const header = document.querySelector(".header");
 mobile_nav.addEventListener("click", () => {
   header.classList.toggle("active");
 });
+
+// Working tabbed button of Portfolio section
+
+const p_btns = document.querySelector(".portfolio-btn");
+const p_btn = document.querySelectorAll(".p-btn");
+const overlay_img = document.querySelectorAll(".img-overlay");
+
+p_btns.addEventListener("click", (e) => {
+  const p_btn_clicked = e.target;
+  console.log(p_btn_clicked);
+
+  p_btn.forEach((element) => element.classList.remove("p-btn-active"));
+
+  p_btn_clicked.classList.add("p-btn-active");
+
+  const btn_num = p_btn_clicked.dataset.btnNum;
+  console.log(btn_num);
+
+  const img_active = document.querySelectorAll(`.p-btn-${btn_num}`);
+
+  overlay_img.forEach((element) => {
+    element.classList.add("p-btn-notActive");
+  });
+
+  img_active.forEach((element) => {
+    element.classList.remove("p-btn-notActive");
+  });
+});
+
+// Animate counter section
+
+const counterNum = document.querySelectorAll(".counter_num");
+
+const speed = 200;
+
+counterNum.forEach((e) => {
+  const update = () => {
+    const targetValue = parseInt(e.dataset.number);
+    console.log(targetValue);
+
+    const initialValue = parseInt(e.innerText);
+    console.log(initialValue);
+
+    const incrementCounter = Math.trunc(targetValue / speed);
+    console.log(incrementCounter);
+
+    if (initialValue < targetValue) {
+      e.innerText = `${initialValue + incrementCounter}+`;
+      setTimeout(update, 10);
+    }
+  };
+  update();
+});
